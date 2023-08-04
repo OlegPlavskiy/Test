59bbee1560da12d6cbe51954e512f800f80842b9
OlegPlavskiy <47296651+OlegPlavskiy@users.noreply.github.com>
2023-08-04 19:51:04 +0400

    Update TestGeneral.js

--- Tests/TestGeneral.js
+++ Tests/TestGeneral.js
@@ -1,8 +1,1 @@
-var expect = require('chai').expect;
 
-describe('My First Test', () => {
-    it('Does not do much!', () => {
-        cy.visit("https://changenow.io/");
-        expect(true).to.equal(true)
-    })
-})
